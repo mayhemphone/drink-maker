a2f56d1ce3472b257e30e8f8af6b3bd117dcb971
mayhemphone <mayhemphone@gmail.com>
2019-04-17 11:38:58 -0700

    stubbed out drinks

--- controllers/drinks.js
+++ controllers/drinks.js
@@ -8,9 +8,23 @@ let router = express.Router()
 // Reference the models
 // let db = require('../models')
 
-// GET /profile
+// GET /drinks
 router.get('/', (req,res)=> {
-	res.send('PROFILE STUB PAGE')
+	res.render('drinks/index')
+})
+
+// POST /drinks
+router.post('/', (req,res)=> {
+	res.send(req.body)
+})
+
+
+router.get('/new', (req,res)=> {
+	res.render('drinks/new')
+})
+
+router.get('/:id', (req,res)=> {
+	res.render('drinks/show')
 })
 
 // EXPORT the routes form this file
