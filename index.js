41644f1705312bef37de07251036578d2fe3d3af
mayhemphone <mayhemphone@gmail.com>
2019-04-17 11:23:44 -0700

    removing parts of boiler

--- index.js
+++ index.js
@@ -33,8 +33,7 @@ app.use((req, res, next) =>{
 })
 
 // Include routes from controllers
-app.use('/auth', require('./controllers/auth'))
-app.use('/profile', require('./controllers/profile'))
+app.use('/drinks', require('./controllers/drinks'))
 
 // make home route
 app.get('/', (req, res)=>{
